3137245542441d0d83844c6c075ec7b362816298
Bushra <bushragemini14@gmail.com>
2021-10-01 17:10:50 +0500

    Created Navbar

---
+++ src/Components/NavBar.js
@@ -0,0 +1,72 @@
+import Carousel from 'react-bootstrap/Carousel'
+import slider1 from '../Images/slider-img1.jpg';
+import slider from '../Images/slider-img.jpg';
+import slider2 from '../Images/slider-img2.jpg';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import logo from '../Images/Circle.png';
+import Nav from 'react-bootstrap/Nav';
+
+
+function NavBar() {
+    return (
+        <>
+            <Navbar bg="light">
+                <Container>
+                    <Navbar.Brand href="#home">
+                        <img
+                            src={logo}
+                            width="30"
+                            height="30"
+                            className="d-inline-block align-top"
+                            alt="React Bootstrap logo"
+                        />
+                    </Navbar.Brand>
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#features">Features</Nav.Link>
+                    <Nav.Link href="#pricing">Pricing</Nav.Link>
+                </Container>
+            </Navbar>
+
+            <Carousel>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={slider1}
+                        alt="First slide"
+                    />
+                    <Carousel.Caption>
+                        <h3>First slide label</h3>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={slider}
+                        alt="Second slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h3>Second slide label</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={slider2}
+                        alt="Third slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h3>Third slide label</h3>
+                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+        </>
+    )
+}
+
+export default NavBar;
